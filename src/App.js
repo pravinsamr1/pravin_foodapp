8908e4257146c61d3264ec089de2ede540d88697
pravinsamr1 <pravinsamr@gmail.com>
2024-10-19 19:06:35 +0530

    This is the first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import Navbar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import Placeorder from './pages/PlaceOrder/PlaceOrder';
+import Footer from './components/Footer/Footer';
+import Login from './components/Login/Login';
 
-function App() {
+const App = () => {
+
+  const[login,setlogin] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {login?<Login setlogin={setlogin} />:<></>}
+    <div className='app'>
+      <Navbar setlogin={setlogin}/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/cart' element={<Cart/>}></Route>
+        <Route path='/order' element = {<Placeorder/>}></Route>
+      </Routes>
     </div>
-  );
+    <Footer/>
+    </>
+  )
 }
 
-export default App;
+export default App
